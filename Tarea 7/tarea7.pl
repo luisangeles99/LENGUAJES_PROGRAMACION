3aef40ef5238aad818100e6e810b53043cf0bbc9
luisangeles99 <A01273884@itesm.mx>
2021-11-27 00:23:35 -0600

    Finish Tarea 7

--- Tarea 7/tarea7.pl
+++ Tarea 7/tarea7.pl
@@ -1,5 +1,5 @@
 % Adriana Fernánde A01197148
-%
+% Luis Angeles A01273884
 % Tarea 7
 
 % Funcion que encuentra el n-ésimo elemento de una lista
@@ -46,3 +46,52 @@ cuenta_profundo(Elem, [_|T], R) :-
     cuenta_profundo(Elem, T, RAux), !,
     R is RAux.
 cuenta_profundo(_,[],0).
+
+% Función lista únicos en Prolog que obtenga una lista con los elementos que no aparecen repetidos dentro de una lista imbricada
+
+lista_unicos_aux([H|T], Prev,[H|R]) :-
+    \+ member(H,T),
+    \+ member(H,Prev),!,
+    lista_unicos_aux(T, Prev,R).
+lista_unicos_aux([H|T], Prev, R) :- append(Prev, [H], NewPrev),
+    lista_unicos_aux(T, NewPrev, R).
+lista_unicos_aux([], _, []).
+
+lista_unicos(L,R) :-
+    flatten(L, RAux), 
+    lista_unicos_aux(RAux, [],R).
+lista_unicos([], []).
+
+
+% Predicado mayores en Prolog que regrese una lista con los elementos mayores que un valor dado en un árbol binario descrito con la función:
+%                               arbol(Raíz, SubárbolIzquierdo, SubárbolDerecho).
+arbol(raiz, subarbolIzquierdo, subarbolDerecho).
+
+
+mayores(Valor, arbol(R, I, D), H):- 
+    Valor < R,!,
+    mayores(Valor, I, Rizq),
+    mayores(Valor, D, Rder),
+    append([R|Rizq], Rder, H).
+mayores(Valor, arbol(R, _, D), H):- 
+    Valor > R,!,
+    mayores(Valor, D, H).
+mayores(_, nil, []).
+
+
+% Predicado siembra en Prolog que a partir de una lista de números cree un árbol binario de búsqueda descrito con la función:
+%                               arbol(Raíz, SubárbolIzquierdo, SubárbolDerecho).
+
+%inserta un valor al bst
+inserta(R, nil, arbol(R,nil, nil)). 
+inserta(R, arbol(R2,I,D), arbol(R2,I2,D)):- R=<R2, inserta(R,I,I2).
+inserta(R, arbol(R2,I,D), arbol(R2,I,D2)):- R>R2, inserta(R,D,D2).
+
+%auxiliar para manejar los casos
+siembra_aux([],nil). /* insert a list of numbers into an empty binary search tree */
+siembra_aux([H|T],Arbol):- siembra_aux(T, Temp), inserta(H,Temp,Arbol),!.
+
+%reverse a la lista por recursividad
+siembra(L, A):-
+    reverse(L, RevL),
+    insert_list(RevL, A).
